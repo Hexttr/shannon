f4d9f54eb93b6af35b21c4ee198afacce7c13a2c
Hex <tmbinfo@mail.ru>
2026-01-31 14:31:48 +0300

    Добавлены недостающие файлы Laravel: bootstrap, public, middleware, providers

--- backend-laravel/app/Providers/AppServiceProvider.php
+++ backend-laravel/app/Providers/AppServiceProvider.php
@@ -3,17 +3,29 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     */
     public function register(): void
     {
         //
     }
 
+    /**
+     * Bootstrap any application services.
+     */
     public function boot(): void
     {
-        //
+        // Используем UUID для моделей
+        \Illuminate\Database\Eloquent\Model::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = (string) Str::uuid();
+            }
+        });
     }
 }
 
