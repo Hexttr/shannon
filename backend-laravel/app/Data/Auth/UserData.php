c3874ff4aea44479bf6a7ebca8e3fc04872f1334
Hex <tmbinfo@mail.ru>
2026-01-31 14:59:55 +0300

    Исправлен UserData для работы с моделью User

--- backend-laravel/app/Data/Auth/UserData.php
+++ backend-laravel/app/Data/Auth/UserData.php
@@ -3,15 +3,19 @@
 namespace App\Data\Auth;
 
 use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Attributes\MapName;
+use Spatie\LaravelData\Mappers\SnakeCaseMapper;
+use DateTime;
 
+#[MapName(SnakeCaseMapper::class)]
 class UserData extends Data
 {
     public function __construct(
         public string $id,
         public string $username,
         public string $email,
-        public ?string $created_at = null,
-        public ?string $updated_at = null,
+        public DateTime $created_at,
+        public DateTime $updated_at,
     ) {
     }
 }
